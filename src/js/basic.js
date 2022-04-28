b2abd4bd796b6b07407edb67aaf765263a2215c3
Dmitry <dmitry.izjurov@yandex.ru>
2022-04-28 19:46:50 +0300

    Выполнено задание Set

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,43 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default class Team {
+  constructor() {
+    this.members = new Set();
+    this.bowman = { name: 'Bowman', type: 'Bowman' };
+    this.swordsman = { name: 'Swordsman', type: 'Swordsman' };
+    this.magician = { name: 'Magician', type: 'Magician' };
+    this.daemon = { name: 'Daemon', type: 'Daemon' };
+    this.undead = { name: 'Undead', type: 'Undead' };
+    this.zombie = { name: 'Zombie', type: 'Zombie' };
+    this.team = [this.bowman, this.swordsman, this.magician, this.daemon, this.undead, this.zombie];
   }
-  return result;
+
+  add(unit) {
+    if (typeof unit === 'string' && this.team.find((a) => a.name === unit)) {
+      const indexUnit = this.team.findIndex((a) => a.name === unit);
+      if (!this.members.has(this.team[indexUnit])) {
+        this.members.add(this.team[indexUnit]);
+        return this.members;
+      }
+      throw new Error('Персонаж уже добавлен в команду!');
+    } else {
+      throw new Error('Персонаж не найден!');
+    }
+  }
+
+  addAll(...units) {
+    units.forEach((arg) => {
+      if (typeof arg === 'string' && this.team.find((a) => a.name === arg)) {
+        const indexUnit = this.team.findIndex((a) => a.name === arg);
+        this.members.add(this.team[indexUnit]);
+      }
+    });
+    return this.members;
+  }
+
+  toArray() {
+    const result = [];
+    for (const unit of this.members) {
+      result.push(unit);
+    }
+    return result;
+  }
 }
